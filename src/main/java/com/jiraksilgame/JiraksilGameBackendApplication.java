87bb94ce6393f6da3e6ecbfcee4318910606301a
jiyoung <aa991102@naver.com>
2025-07-24 14:07:05 +0900

    init: 프로젝트 구조 생성 및 JPA, Security, JWT 의존성 추가

---
+++ src/main/java/com/jiraksilgame/JiraksilGameBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jiraksilgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiraksilGameBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiraksilGameBackendApplication.class, args);
+	}
+
+}
