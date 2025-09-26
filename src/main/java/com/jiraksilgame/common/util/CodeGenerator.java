e6fa07da9c2e3da1ee575302de304778d46bbc02
jiyoung <aa991102@naver.com>
2025-09-27 01:10:42 +0900

    docs(common): CodeGenerator 주석 정리

--- src/main/java/com/jiraksilgame/common/util/CodeGenerator.java
+++ src/main/java/com/jiraksilgame/common/util/CodeGenerator.java
@@ -3,7 +3,7 @@ package com.jiraksilgame.common.util;
 import java.security.SecureRandom;
 
 /**
- * 외부 노출용 게임 코드/토큰 등 짧은 랜덤 코드 생성
+ * 게임 코드 생성 유틸
  */
 public final class CodeGenerator {
     private static final char[] ALPHANUM = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".toCharArray();
@@ -12,11 +12,11 @@ public final class CodeGenerator {
     private CodeGenerator() {}
 
     /**
-     * 대문자·숫자 조합의 난수 코드 생성
+     * 주어진 길이(len)만큼 랜덤 코드 생성
      * <p>SecureRandom 사용, 고유성은 호출 측에서 보장</p>
      * 
-     * @param len 생성 길이
-     * @return 생성된 코드
+     * @param len 생성할 코드 길이
+     * @return 생성된 코드(A~Z, 0~9로 구성된 랜덤 문자열)
      */
     public static String randomCode(int len) {
         char[] buf = new char[len];
